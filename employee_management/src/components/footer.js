e9251333351ad67dabd163cf3d7d24e89680678d
Aman Kaur <amankaur@Amans-MacBook-Pro.local>
2021-06-19 11:10:40 -0700

    Adding footer component

---
+++ employee_management/src/components/footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+// import "react-bootstrap";
+import { BrowserRouter as Link } from "react-router-dom";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div class="displayNav">
+        <div className="footer ">
+          <p>Footer we can add stuff if we want</p>
+        </div>
+      </div>
+    );
+  }
+}
