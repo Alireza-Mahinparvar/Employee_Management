a0e41c8ccf1723968368d9bb3219406348be2bd3
Princej258 <74393262+Princej258@users.noreply.github.com>
2021-07-19 22:24:42 -0700

    my version

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
+import Homepage from "./homepage";
+
+function App() {
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="nav-link" to={"/homepage"}>E-Management</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="outer">
+        <div className="inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route path="/homepage" component={Homepage} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/homepage.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import './Homepage.css';
+// import './Homepage.jpeg' Image_1;
+
+export default class Homepage extends Component {
+    render() {
+        return (
+            <frontpage>
+
+                <h3>The Tomorrow of Management!</h3>
+                
+                <p>The new way of Employee Management</p>
+
+                <p3>Allows Employees more control over their work! </p3>
+                <image>"./homepage.jpeg"</image>
+                <image>import { './homepage.jpeg' } from "./homepage.jpeg" </image>
+                
+                </frontpage>
+        );
+    }
+}
