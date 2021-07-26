1f605fd641f5f37f708a79eb00c9e46e0c0e6eb7
ElizabethNa7 <63435879+ElizabethNa7@users.noreply.github.com>
2021-07-25 18:19:23 -0700

    frontend layout done(?)
    need to figure out how to make it more connected in the back (like how to take the selected Role and Task as input etc)

--- client/employee_management/src/components/Emails.js
+++ client/employee_management/src/components/Emails.js
@@ -1,109 +1,115 @@
-//work in progress
-
 import React, { Component, useState } from "react";
+import "./tasks.css";
 
 // import Navigation from "./navigation";
 import Footer from "./footer.js";
-import "./tasks old.css";
+import "./pageLayout.css";
 import RecordList from "./recordList.js";
 
 function Emails() {
-  const [mailerState, setMailerState] = useState({
-    email: "",
-    subject: "",
-    message: "",
-  });
-
-  function handleStateChange(e) {
-    setMailerState((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  }
-
-  const submitEmail = async (e) => {
-    e.preventDefault();
-    console.log({ mailerState });
-    const response = await fetch("http://localhost:3001/send", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ mailerState }),
-    })
-      .then((res) => res.json())
-      .then(async (res) => {
-        const resData = await res;
-        console.log(resData);
-        if (resData.status === "success") {
-          alert("Message Sent :)");
-        } else if (resData.status === "fail") {
-          alert("Message failed to send :(");
-        }
-      })
-      .then(() => {
-        setMailerState({
-          email: "",
-          subject: "",
-          message: "",
-        });
-      });
-  };
-
   return (
-    <div>
-      {/* <Navigation /> */}
-      <form
-        style={{
-          display: "flex",
-          //   height: "100vh",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        onSubmit={submitEmail}
-        className="container-email"
-      >
-        <fieldset
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "left",
-            width: "100%",
-          }}
-        >
-          <legend>E-mail Station</legend>
-          <input
-            placeholder="Email"
-            onChange={handleStateChange}
-            name="email"
-            value={mailerState.email}
-          />
-          <input
-            placeholder="Subject"
-            onChange={handleStateChange}
-            name="subject"
-            value={mailerState.subject}
-          />
-          <br />
-          <textarea
-            placeholder="Message"
-            onChange={handleStateChange}
-            name="message"
-            value={mailerState.message}
-          />
-          <br />
-          <button className="btn btn-outline-info">Send Message</button>
-        </fieldset>
+    <div className="todoapp stack-large">
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            Assign a task
+          </label>
+        </h2>
+        <select class="ddbutton">
+  <option value="1" selected="selected">Role</option>
+  <option value="2">JS programmer</option>
+  <option value="3">React programmer</option>
+  <option value="4">Program manager</option>
+</select>
+<select class="ddbutton">
+  <option value="1" selected="selected">Task</option>
+  <option value="2">Commit changes</option>
+  <option value="3">Review request</option>
+  <option value="4">Plan weekly sprints</option>
+</select>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
       </form>
-
-      <legend class="container-userList">
-        {" "}
-        Users list to send email to?
-        {<RecordList />}
-      </legend>
-
-      <Footer />
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-0" type="checkbox" defaultChecked={true} />
+            <label className="todo-label" htmlFor="todo-0">
+              Commit changes
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Eat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Eat</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-1" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-1">
+              Review request
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Sleep</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Sleep</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-2" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-2">
+              Plan weekly sprints
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Repeat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Repeat</span>
+            </button>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
-export default Emails;
+ export default Emails;
