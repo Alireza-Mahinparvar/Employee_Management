8f33b525430b0947ae193bd080a391e2d9d419ec
ElizabethNa7 <63435879+ElizabethNa7@users.noreply.github.com>
2021-07-28 21:59:38 -0700

    newest version

--- client/employee_management/src/Emails.js
+++ client/employee_management/src/Emails.js
@@ -1,208 +1,77 @@
 import React, { Component, useState } from "react";
 import "./tasks.css";
+import Form from "./Form";
+import FilterButton from "./FilterButton";
+import TaskList from "./taskscomponent.js";
+import "./../index.js";
+import { nanoid } from "nanoid";
+//import "./../App.js";
 
 // import Navigation from "./navigation";
 import Footer from "./footer.js";
 import "./pageLayout.css";
 import RecordList from "./recordList.js";
 
-function Emails() {
-  const [mailerState, setMailerState] = useState({
-    email: "",
-    subject: "",
-    message: "",
-  });
-  
-  function handleStateChange(e) {
-    setMailerState((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
+
+function Emails(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   }
-  
-  const submitEmail = async (e) => {
-    e.preventDefault();
-    console.log({ mailerState });
-    const response = await fetch("http://localhost:3001/send", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ mailerState }),
-    })
-      .then((res) => res.json())
-      .then(async (res) => {
-        const resData = await res;
-        console.log(resData);
-        if (resData.status === "success") {
-          alert("Message Sent");
-        } else if (resData.status === "fail") {
-          alert("Message failed to send");
-        }
-      })
-      .then(() => {
-        setMailerState({
-          email: "",
-          subject: "",
-          message: "",
-        });
-      });
-  };
-  
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map(task => (
+    <TaskList
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
-    <div>
-      {/* <Navigation /> */      }
-      <form
-        style={{
-          display: "flex",
-          //   height: "100vh",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        onSubmit={submitEmail}
-        className="container-issue-tasks"
-      >
-        <fieldset
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "left",
-            width: "100%",
-          }}
-        >
-          <legend>Issue Tasks</legend>
-          <input class="role"
-            placeholder="Role"
-            onChange={handleStateChange}
-            name="email"
-            value={mailerState.email}
-          />
-          <input class ="task"
-            placeholder="Task"
-            onChange={handleStateChange}
-            name="subject"
-            value={mailerState.subject}
-          />
-          <br />
-          <textarea class="comments"
-            placeholder="Comments"
-            onChange={handleStateChange}
-            name="message"
-            value={mailerState.message}
-          />
-          <br />
-          <button class="btn btn-outline-info">Submit</button>
-        </fieldset>
-      </form>
-  
-      <legend class="container-taskList"> Task Statuses</legend>
-      
-      
-
-
-
-
-
-
-      <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+    <div className="todoapp stack-large">
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
+
+      <h2 id="list-heading">{headingText}</h2>
+
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-0" type="checkbox" defaultChecked={true} />
-            <label className="todo-label" htmlFor="todo-0">
-              Eat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Eat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Eat</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-1" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-1">
-              Sleep
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Sleep</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Sleep</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-2" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-2">
-              Repeat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Repeat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Repeat</span>
-            </button>
-          </div>
-        </li>
+        {taskList}
       </ul>
     </div>
-
-
-
-      <Footer />
-    </div>
   );
- }
+}
  export default Emails;
