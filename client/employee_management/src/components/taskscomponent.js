7ef0430d12bbf8bef80520895efa814ee26aa26b
ElizabethNa7 <63435879+ElizabethNa7@users.noreply.github.com>
2021-07-29 23:25:10 -0700

    edited to add edit and task status functionalities

--- client/employee_management/src/components/taskscomponent.js
+++ client/employee_management/src/components/taskscomponent.js
@@ -1,32 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function TaskList(props) {
-  return (
-    <li className="todo stack-small">
-      <div className="c-cb">
-      <input
-  id={props.id}
-  type="checkbox"
-  defaultChecked={props.completed}
-  onChange={() => props.toggleTaskCompleted(props.id)}
-/>
-        <label className="todo-label" htmlFor={props.id}>
-          {props.name}
+  const [isEditing, setEditing] = useState(false);
+  
+  const [newName, setNewName] = useState('');
+
+  function handleChange(e) {
+    setNewName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.editTask(props.id, newName);
+    setNewName("");
+    setEditing(false);
+  }
+
+  const editingTemplate = (
+    <form className="stack-small" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="taskList-label" htmlFor={props.id}>
+          New name for {props.name}
         </label>
+        <input
+  id={props.id}
+  className="taskList-text"
+  type="text"
+  value={newName}
+  onChange={handleChange}
+/>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn">
-          Edit <span className="visually-hidden">{props.name}</span>
-        </button>
-        <button
+      <button
   type="button"
-  className="btn btn__danger"
-  onClick={() => props.deleteTask(props.id)}
+  className="btn taskList-cancel"
+  onClick={() => setEditing(false)}
 >
-  Delete <span className="visually-hidden">{props.name}</span>
+  Cancel
+  <span className="visually-hidden">renaming {props.name}</span>
 </button>
+        <button type="submit" className="btn btn__primary taskList-edit">
+          Save
+          <span className="visually-hidden">new name for {props.name}</span>
+        </button>
       </div>
-    </li>
+    </form>
   );
+  const viewTemplate = (
+    <div className="stack-small">
+      <div className="c-cb">
+          <input
+            id={props.id}
+            type="checkbox"
+            defaultChecked={props.completed}
+            onChange={() => props.toggleTaskCompleted(props.id)}
+          />
+          <label className="todo-label" htmlFor={props.id}>
+            {props.name}
+          </label>
+        </div>
+        <div className="btn-group">
+        <button type="button" className="btn" onClick={() => setEditing(true)}>
+  Edit <span className="visually-hidden">{props.name}</span>
+</button>
+          <button
+            type="button"
+            className="btn btn__danger"
+            onClick={() => props.deleteTask(props.id)}
+          >
+            Delete <span className="visually-hidden">{props.name}</span>
+          </button>
+        </div>
+    </div>
+  );
+
+
+  return <li className="TaskList">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
