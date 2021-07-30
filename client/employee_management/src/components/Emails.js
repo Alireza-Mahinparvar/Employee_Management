8b6dbad81172396ca5c8b298265264b279017c17
ElizabethNa7 <63435879+ElizabethNa7@users.noreply.github.com>
2021-07-29 23:25:29 -0700

    edited to add edit and task status functionalities

--- client/employee_management/src/components/Emails.js
+++ client/employee_management/src/components/Emails.js
@@ -12,9 +12,19 @@ import Footer from "./footer.js";
 import "./pageLayout.css";
 import RecordList from "./recordList.js";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function Emails(props) {
   const [tasks, setTasks] = useState(props.tasks);
+
+  const [filter, setFilter] = useState('All');
+
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task => {
@@ -29,12 +39,26 @@ function Emails(props) {
     setTasks(updatedTasks);
   }
 
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
   function deleteTask(id) {
     const remainingTasks = tasks.filter(task => id !== task.id);
     setTasks(remainingTasks);
   }
 
-  const taskList = tasks.map(task => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
     <TaskList
       id={task.id}
       name={task.name}
@@ -42,6 +66,16 @@ function Emails(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
 
@@ -57,9 +91,7 @@ const headingText = `${taskList.length} ${tasksNoun} remaining`;
     <div className="todoapp stack-large">
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
 
       <h2 id="list-heading">{headingText}</h2>
@@ -74,4 +106,4 @@ const headingText = `${taskList.length} ${tasksNoun} remaining`;
     </div>
   );
 }
- export default Emails;
+ export default Emails; 
